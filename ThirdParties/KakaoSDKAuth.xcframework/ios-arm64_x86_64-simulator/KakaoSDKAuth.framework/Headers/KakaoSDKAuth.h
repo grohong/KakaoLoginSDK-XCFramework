95fbb84a1283f4ef620d054cc1c8ed1596160956
홍성호 <sh.hong@woowahan.com>
2022-08-17 17:59:13 +0900

    Kakao 버전 업데이트

--- ThirdParties/KakaoSDKAuth.xcframework/ios-arm64_x86_64-simulator/KakaoSDKAuth.framework/Headers/KakaoSDKAuth.h
+++ ThirdParties/KakaoSDKAuth.xcframework/ios-arm64_x86_64-simulator/KakaoSDKAuth.framework/Headers/KakaoSDKAuth.h
@@ -21,3 +21,5 @@ FOUNDATION_EXPORT double KakaoSDKAuthVersionNumber;
 FOUNDATION_EXPORT const unsigned char KakaoSDKAuthVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <KakaoSDKAuth/PublicHeader.h>
+
+
